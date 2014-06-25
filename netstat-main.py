8f509bd4bc43621b664a73ba26be13a9c55d616f
psjoshi <joshi.pradyumna@gmail.com>
2014-06-25 15:45:59 +0530

    Adding e-mail support for blacklisted IPs/Countries

--- netstat-main.py
+++ netstat-main.py
@@ -18,7 +18,7 @@ from IPy import IP
 from dnsbl import DNSBL_check
 # virustotal checks
 from virustotal import Virustotal
-
+import time
 
 def setup_logging():
 	""" set up logging"""
@@ -86,6 +86,9 @@ def config_options(config_file, log_instance):
 		connections_type = _confGet(conf, "settings", "connections_type") or None
 		virustotal_key = _confGet(conf, "settings", "virustotal_key") or None
 		refresh_interval = _confGet(conf, "settings", "refresh_interval") or 5
+		blacklist_countries = _confGet(conf,"settings","blacklist_countries") or None
+		blacklist_ips = _confGet(conf,"settings","blacklist_ips") or None
+
 		# proxy
 		proxy_user = _confGet(conf, "proxy", "user") or None
 		proxy_password = _confGet(conf, "proxy", "password") or None
@@ -95,8 +98,15 @@ def config_options(config_file, log_instance):
 		country_db = _confGet(conf, "GeoIP_databases", "country_database") or None
 		city_db = _confGet(conf, "GeoIP_databases", "city_database") or None
 
+		# e-mail
+		email_user = _confGet(conf,"e-mail","user") or None
+		email_password = _confGet(conf,"e-mail","password") or None
+		email_server = _confGet(conf,"e-mail","server") or None
+		email_port = _confGet(conf,"e-mail","port") or None
+
 		return connections_type, virustotal_key, refresh_interval, city_db, country_db, asn_db, \
-		       proxy_user, proxy_password, proxy_server, proxy_port
+		       proxy_user, proxy_password, proxy_server, proxy_port, email_user,email_password,email_server,email_port, \
+			   blacklist_countries, blacklist_ips
 
 	except Exception, e:
 		log_instance.error('Error while reading configuration file - %s' % str(e).strip(), exc_info=True)
@@ -206,6 +216,43 @@ def check_ip_using_virustotal(log_instance, api_key, user, password, server, por
 		log_instance.error("Error while checking IP against Virustotal database-%s" % str(e).strip())
 
 
+def mail_to_admin(log_instance, malware_contents, mail_user, mail_passwd, mail_server, mail_port):
+	import smtplib, base64
+	from email.mime.multipart import MIMEMultipart
+	# Import the email modules we'll need
+	from email.mime.text import MIMEText
+
+
+	if malware_contents:
+		content = '\n'.join(malware_contents)
+
+		from_mail = mail_user + '@' + mail_server
+		to_mail = mail_user + '@' + mail_server
+		msg = MIMEMultipart('alternative')
+		msg['Subject'] = 'Detection of blacklisted IP connections on your PC'
+		#msg['From'] = from_mail
+		#msg['To'] = ','.join(to_mail)
+		msg['From'] = mail_user + '@' + mail_server
+		msg['To'] = mail_user + '@' + mail_server
+		part1 = MIMEText(content, 'plain')
+		#part2 = MIMEText(main_msg_html,'html')
+		msg.attach(part1)
+		#msg.attach(part2)
+	try:
+		so = smtplib.SMTP(mail_server)
+		so.docmd("AUTH LOGIN", base64.b64encode(mail_user))
+		so.docmd(base64.b64encode(mail_passwd), "")
+		#so.login(config.email_user,config.email_passwd)
+		try:
+			so.sendmail(from_mail, to_mail, msg.as_string())
+		except Exception, e:
+			log_instance.error("Error while sending E-mail about suspicious IP connections - %s" % str(e))
+		finally:
+			so.close()
+	except Exception, e:
+		log_instance.error("Unable to send E-mail about suspicious IP connections - %s" % str(e))
+
+
 if __name__ == '__main__':
 	try:
 		# setup logging
@@ -215,7 +262,8 @@ if __name__ == '__main__':
 		# check if config file exists. if yes, read configuaration parameters
 		if os.path.isfile(cmd_args.config_file):
 			connections_type, virustotal_key, refresh_interval, city_db, country_db, asn_db, proxy_user, proxy_password, \
-			proxy_server, proxy_port = config_options(cmd_args.config_file, log_instance)
+			proxy_server, proxy_port, email_user,email_password,email_server,email_port,blacklist_countries, \
+			blacklist_ips = config_options(cmd_args.config_file, log_instance)
 
 		if not (asn_db and country_db and city_db):
 			log_instance.warning("Some of the Geo-databases do not exists. All the network connection information \
@@ -229,6 +277,9 @@ if __name__ == '__main__':
 		print "Network connection details:"
 		print "{0:15} {1:12} {2:15} {3:12} {4:20} {5:20} {6:20}".format("Local addr", "Local port", "Remote addr", \
 				"Remote port", "Country", "Virustotal status", "DNSBL status")
+
+		blacklisted_country_connections = []
+		blacklisted_ip_connections = []
 
 		for item in network_connections:
 			# check if remote address is valid and is not private
@@ -268,9 +319,32 @@ if __name__ == '__main__':
 						print "{0:15} {1:12} {2:15} {3:12} {4:20} {5:20} {6:20}".format(str(item.laddr[0]),str(item.laddr[1]),str(item.raddr[0]), \
 						    str(item.raddr[1]),ip_country, str(virustotal_result),str(dnsbl_result))
 
+						# tracking blacklisted country connections
+						if blacklist_countries:
+							if ip_country in blacklist_countries:
+								country_entry="Process %s tried to connect to remote IP %s in Country %s at %s" %(p.exe,\
+								item.raddr[0],ip_country, time.strftime("%d-%m-%Y %H:%M"))
+								blacklisted_country_connections.append(country_entry)
+
+						# tracking blacklisted ip connections
+						if blacklist_ips:
+							if item.raddr[0] in blacklist_ips:
+								ip_entry="Process %s tried to connect to remote IP %s in Country %s at %s" %(p.exe,\
+								item.raddr[0],ip_country, time.strftime("%d-%m-%Y %H:%M"))
+								blacklisted_ip_connections.append(country_entry)
+
 						# print item.laddr[0], item.laddr[1],item.raddr[0],item.raddr[1],item.status,item.pid,p.exe(), \
 						# p.cpu_percent(),ip_asn,ip_country,ip_city,virustotal_result,dnsbl_result
 
-        except Exception, e:
-            log_instance.error('The network connections can not be displayed because of error- %s' % str(e).strip(),
-                           exc_info=True)
+		# send a mail if blacklisted connections are found
+		if blacklisted_country_connections:
+			mail_to_admin(log_instance, blacklisted_country_connections, email_user, email_password, email_server, \
+			              email_port)
+
+		if blacklisted_ip_connections:
+			mail_to_admin(log_instance, blacklisted_ip_connections, email_user, email_password, email_server, \
+			              email_port)
+
+	except Exception, e:
+		log_instance.error('The network connections can not be displayed because of error- %s' % str(e).strip(),
+                   exc_info=True)
